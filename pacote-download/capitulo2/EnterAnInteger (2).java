989296a61f2730c738160bd494df6cc663060e23
irion-silva <irion.silva@hotmail.com>
2021-09-04 16:28:19 -0300

    Initial Commit

--- pacote-download/capitulo2/EnterAnInteger (2).java
+++
@@ -1,9 +0,0 @@
-/* b) Solicite que o usuário insira um inteiro.
-*/
-package capitulo2;
-public class EnterAnInteger {
-    public static void main(String[] args) {
-        System.out.print("Enter an Integer.");
-    }
-    
-}
